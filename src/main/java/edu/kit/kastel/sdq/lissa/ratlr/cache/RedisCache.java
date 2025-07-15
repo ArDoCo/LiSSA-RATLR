ad7fd6c49ee172a8019e5ae13bc917d2428710e5
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-07-15 15:37:42 +0200

    Ensure correct handling of cachekeys

--- src/main/java/edu/kit/kastel/sdq/lissa/ratlr/cache/RedisCache.java
+++ src/main/java/edu/kit/kastel/sdq/lissa/ratlr/cache/RedisCache.java
@@ -94,11 +94,11 @@ class RedisCache implements Cache {
      */
     @Override
     public synchronized <T> T get(CacheKey key, Class<T> clazz) {
-        var jsonData = jedis == null ? null : jedis.hget(key.toRawKey(), "data");
+        var jsonData = jedis == null ? null : jedis.hget(key.toJsonKey(), "data");
         if (jsonData == null && localCache != null) {
             jsonData = localCache.get(key);
             if (jedis != null && jsonData != null) {
-                jedis.set(key.toRawKey(), jsonData);
+                jedis.set(key.toJsonKey(), jsonData);
             }
         }
 
@@ -142,7 +142,7 @@ class RedisCache implements Cache {
     @Override
     public synchronized void put(CacheKey key, String value) {
         if (jedis != null) {
-            String rawKey = key.toRawKey();
+            String rawKey = key.toJsonKey();
             jedis.hset(rawKey, "data", value);
             jedis.hset(rawKey, "timestamp", String.valueOf(Instant.now().getEpochSecond()));
         }
